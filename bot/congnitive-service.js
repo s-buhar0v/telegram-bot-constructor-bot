b128d355330c13cb297a41885a0b37ccc74f11cc
Sergey Bukharov <s.buharov@dodopizza.com>
2018-03-18 15:19:32 +0300

    config fix

--- bot/congnitive-service.js
+++ bot/congnitive-service.js
@@ -2,11 +2,11 @@ const axios = require('axios')
 
 const config = require('../config')
 
-axios.defaults.headers.common['Ocp-Apim-Subscription-Key'] = apiKey
+axios.defaults.headers.common['Ocp-Apim-Subscription-Key'] = config.cognitiveServices.apiKey
 
 function findTextMessageAnswer(message, callback) {
-    axios.get(`${webSearchHost}?q=${encodeURIComponent(message)}`).then(textResponse => {
-        axios.get(`${imageSearchHost}?q=${encodeURIComponent(message)}`).then(imageResponse => {
+    axios.get(`${config.cognitiveServices.webSearchHost}?q=${encodeURIComponent(message)}`).then(textResponse => {
+        axios.get(`${config.cognitiveServices.imageSearchHost}?q=${encodeURIComponent(message)}`).then(imageResponse => {
             callback({
                 answerText: textResponse.data.webPages.value[0].snippet,
                 imageUrl: imageResponse.data.value[0].contentUrl
