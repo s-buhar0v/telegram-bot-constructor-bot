ecd52a1a4d276630a488459922fc6b579b62ce98
Sergey Bukharov <s.buharov@dodopizza.com>
2018-03-18 15:10:57 +0300

    config fox

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const config = {
     botConstructorApiUrl: 'https://bot-constructor-api.azurewebsites.net/api',
     cognitiveServices: {
         apiKey: 'd10c724a96da405e9b7ac4b725852007',
-        webSearchHost = 'https://api.cognitive.microsoft.com/bing/v7.0/search',
-        imageSearchHost = 'https://api.cognitive.microsoft.com/bing/v7.0/images/search',
+        webSearchHost: 'https://api.cognitive.microsoft.com/bing/v7.0/search',
+        imageSearchHost: 'https://api.cognitive.microsoft.com/bing/v7.0/images/search',
     }
 }
 
